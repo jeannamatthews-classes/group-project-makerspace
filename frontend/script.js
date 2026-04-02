73466845ad7d632efde8b78b1e4819f9ce0a667d
Justin P Bouchard <jbouchard18@outlook.com>
2026-04-02 16:36:10 -0400

    Registration form is looking
    The frontend for the registration form should be looking good. I have not yet connected it to flask though, I may need some help with that.

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,51 @@
 const name = document.getElementById('name');
 const email = document.getElementById('email');
 const studentid = document.getElementById('studentid');
-const registrationform = document.getElementById('registrationform');
+const registrationForm = document.getElementById('registrationForm');
 
-registrationform.addEventListener('submit', (e) => {
-    // e.preventDefault();
-    document.getElementById("message").innerHTML = "The form has been submitted (but not yet uploaded).";
+// controls the submit button
+registrationForm.addEventListener('submit', (e) => {
+
+    // reset the errors
+    resetText("nameError");
+    resetText("emailError");
+    resetText("studentidError");
+
+    errors = 0;
+
+    tempEmail = email.value;
+    if (tempEmail.substring(tempEmail.length - 13, tempEmail.length) != "@clarkson.edu") {
+        document.getElementById("emailError").textContent = "Enter a valid Clarkson email";
+        errors++;
+    }
+
+    // check that the student id has the correct length
+    if (studentid.value.length != 7) {
+        document.getElementById("studentidError").textContent = "Enter a valid student ID";
+        errors++;
+    }
+
+
+
+    //check for successful response. If inputs are valid AND response is successful:
+    // TODO: add succesful response condition
+    if (errors === 0) {
+        alert("The form has been submitted (but not yet uploaded).");
+
+        // reset the form
+        document.getElementById("registrationForm").reset();
+
+    }
+
+    // check for successful response. If response is unsuccessful:
+    // TODO: add unsuccessful response condition
+    if (false) {
+        alert("The form was not uploaded. Please try again later.");
+    }
+
 })
 
+// function to reset the errors
+function resetText(fieldName) {
+    document.getElementById(fieldName).textContent = "";
+}
